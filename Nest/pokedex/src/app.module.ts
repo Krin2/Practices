fee1e2f43525eb4c5042c22c6f85108c972e54c2
Marcos Oscar Frigerio <marcos.frigerio@intive.com>
2022-09-08 16:47:21 -0300

    Conectar la base de datos de mongoDB a nuestra aplicacion de Nest

--- Nest/pokedex/src/app.module.ts
+++ Nest/pokedex/src/app.module.ts
@@ -1,13 +1,18 @@
+// Se suelen ordenar los paketes por importacnia
 import { join } from 'path'; // Los paquetes de node suelen ir primero
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
+    // Agregado de un prefijo "global" a todas las rutas
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    // Se agrega la referencia a la base de datos
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokedex'),
     PokemonModule,
   ],
 })
