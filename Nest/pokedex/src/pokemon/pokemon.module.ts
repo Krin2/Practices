000abe71c062443a70e5dfb320689d86bbf23eef
Marcos Oscar Frigerio <marcos.frigerio@intive.com>
2022-09-09 09:43:02 -0300

    Creacion del schema con mongoose e insersion de la tabla pokemon dentro de la DB, decoradores para las propiedades de la tabla

--- Nest/pokedex/src/pokemon/pokemon.module.ts
+++ Nest/pokedex/src/pokemon/pokemon.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 @Module({
   controllers: [PokemonController],
-  providers: [PokemonService]
+  providers: [PokemonService],
+  imports: [
+    // importo el mongoose pero esta vez no es forRoot porque ese solo se ejecuta en el root de la aplicacion
+    MongooseModule.forFeature([
+      {
+        // El modelo tiene 2 propiedades a definir
+        name: Pokemon.name,
+        schema: PokemonSchema,
+      },
+    ]),
+  ],
 })
 export class PokemonModule {}
