165bd8ceb7c6ddd584616d24d3f78c508b3198a0
Marcos Oscar Frigerio <marcos.frigerio@intive.com>
2022-09-13 13:15:54 -0300

    agregado de axios y consulta a la url de pokeapi para obtener los datos de los pokemons

--- Nest/pokedex/src/seed/seed.service.ts
+++ Nest/pokedex/src/seed/seed.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return `Seed executed`;
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    // Toma los datos de la url de pokemonapi con el filtro de 650 pokemons
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=2',
+    );
+
+    data.results.forEach(({ name, url }) => {
+      // En este punto tenemos destructurado los resultados.
+      // Para obtener el numero del pokemon, lo sacamos de la url
+      const segment = url.split('/');
+      const no: number = +segment[segment.length - 2]; // El + transforma el string en number
+      console.log({ name, no });
+    });
+
+    return data.results;
   }
 }
