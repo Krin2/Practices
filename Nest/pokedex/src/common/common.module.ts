455afd1c0de827c1d8c99698348a3c6f477997a8
Marcos Oscar Frigerio <marcos.frigerio@intive.com>
2022-09-19 09:17:58 -0300

    Separacion de este proyecto a un repo nuevo para poder deployarlo en heroku

--- Nest/pokedex/src/common/common.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AxiosAdapter } from './adapters/axios.adapter';
-
-@Module({
-  providers: [AxiosAdapter], // Indica los inyectables que va a usar el módulo
-  exports: [AxiosAdapter], // Hace visible el AxiosAdapter al resto de los módulos
-})
-export class CommonModule {}
